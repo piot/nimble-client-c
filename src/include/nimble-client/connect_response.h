b36b9a9d19e77ef4693986093ff4207243029808
Peter Bjorklund <piot@hotmail.com>
2023-09-13 21:33:37 +0200

    chore: add missing headers

--- src/include/nimble-client/connect_response.h
+++ src/include/nimble-client/connect_response.h
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------------
-*  Copyright (c) Peter Bjorklund. All rights reserved.
-*  Licensed under the MIT License. See LICENSE in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #ifndef NIMBLE_CLIENT_CONNECT_RESPONSE_H
 #define NIMBLE_CLIENT_CONNECT_RESPONSE_H
 
--- src/lib/connection_quality.c
+++ src/lib/connection_quality.c
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #include <nimble-client/client.h>
 #include <nimble-client/connection_quality.h>
 
